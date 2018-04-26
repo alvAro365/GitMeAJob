579e3339463a63334b6f1a819a18e3f05e314f52
Tobias Johansson <TobiasJohansson911@gmail.com>
2018-04-26 17:40:20 +0200

    Custom cell created and connected to customCellController.swift, but not inserted in tableviewController.

---
+++ GitMeAJob/CustomTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  CustomTableViewCell.swift
+//  GitMeAJob
+//
+//  Created by lösen är 0000 on 2018-04-26.
+//  Copyright © 2018 Alvar Aronija. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var jobTitleLabel: UILabel!
+    @IBOutlet weak var someLabel: UILabel!
+    @IBOutlet weak var companyLabel: UILabel!
+    @IBOutlet weak var fullTimeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
